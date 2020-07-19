eac8a47ca37abb35f99c6df0eba4cc857a3375ff
ElderHorse <kcwong395@gmail.com>
2020-07-19 20:59:14 +0800

    fix new display bug

--- src/component/Intro/News/NewsSection.js
+++ src/component/Intro/News/NewsSection.js
@@ -4,10 +4,12 @@ import NewsItem from "./NewsItem"
 import news from "../../../assets/news.json"
 
 function NewsSection() {
+
     return (
+        news.length > 0 &&
         <div>
             <h3 className="header font-pf">Latest News</h3>
-            {news.slice(0, 3).map(item => <NewsItem props={item} />)}
+            {news.slice(0, 3).map(item => <NewsItem content={item.content} />)}
         </div>
     )
 }
