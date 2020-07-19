cb16a3932e5589d3f6baa9ba4bdec2b42fa54c3a
ElderHorse <kcwong395@gmail.com>
2020-07-19 23:50:44 +0800

    no message

---
+++ precache-manifest.6db526d04850847d2f50cc5050c903cb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5ead64d9561b693e00378cf07c5c1bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6a22017beb9d45c2c81",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "4259d25a84ab50842487",
+    "url": "/static/css/main.87842e03.chunk.css"
+  },
+  {
+    "revision": "e6a22017beb9d45c2c81",
+    "url": "/static/js/2.f123b497.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.f123b497.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4259d25a84ab50842487",
+    "url": "/static/js/main.0104387f.chunk.js"
+  },
+  {
+    "revision": "77774284f374a7dc8949",
+    "url": "/static/js/runtime-main.53c7b5a6.js"
+  },
+  {
+    "revision": "16c40e2e729e5ac76a34646f55346b3a",
+    "url": "/static/media/bg.16c40e2e.jpg"
+  },
+  {
+    "revision": "0b8cd1c0ccf160aea726caf704be6351",
+    "url": "/static/media/recent_pic.0b8cd1c0.jpg"
+  },
+  {
+    "revision": "629df1ad44ab4cb27e631501c9d08361",
+    "url": "/static/media/resume.629df1ad.pdf"
+  },
+  {
+    "revision": "1e28affa59b7a024ac7187e4f3b42485",
+    "url": "/static/media/somethingwild-Regular.1e28affa.otf"
+  }
+]);
