02feaea1973b6c976ddfd3654391b25feb2897d4
Martin Wong <kcwong395@gmail.com>
2021-11-13 09:38:09 -0500

    add weight 400 of font and apply diff weight to typograph

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,47 @@
 import React from 'react';
 import './App.css';
 import Home from './views/Home'
-import {createTheme, CssBaseline, ThemeProvider} from '@mui/material';
+import {createTheme, ThemeProvider} from '@mui/material/styles';
+import {CssBaseline} from '@mui/material'
 
 const theme = createTheme({
   typography: {
-    fontFamily: [
-      'Nunito',
-      'sans-serif'
-    ].join(','),
+    fontFamily: ['Nunito', 'sans-serif'].join(','),
+    h1: {
+      fontWeight: 700
+    },
+    h2: {
+      fontWeight: 700
+    },
+    h3: {
+      fontWeight: 700
+    },
+    h4: {
+      fontWeight: 700
+    },
+    h5: {
+      fontWeight: 700
+    },
+    h6: {
+      fontWeight: 700
+    }
   },
   palette: {
-    mode: 'dark',
-  },
+    primary: {
+      main: "#efc8b1"
+    },
+    secondary: {
+      main: "#514644"
+    },
+    mode: "dark"
+  }
 });
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <React.Fragment>
-        <CssBaseline />
-        <Home />
-      </React.Fragment>
+      <CssBaseline />
+      <Home />
     </ ThemeProvider>
   );
 }
