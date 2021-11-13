84ab1f4b540863b3c44970bbb98db82fcad659f3
Martin Wong <kcwong395@gmail.com>
2021-11-13 09:44:54 -0500

    add home page

---
+++ src/views/Home.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Grid} from "@mui/material"
+
+import './Home.css'
+
+import Navbar from '../components/Navbar'
+import ContactBar from '../components/contact/ContactBar'
+import Banner from '../components/Banner'
+
+function Home() {
+  return (
+    <Grid container direction="column" justifyContent="space-between" id={'Section-Home'}>
+      <Grid item>
+        <Navbar />
+      </Grid>
+      <Grid container justifyContent="flex-end" alignItems="center">
+        <Grid item sx={{ pr: 40, pb: 20 }}>
+          <Banner />
+        </Grid>
+      </Grid>
+      <Grid item sx={{ pl: 7, pb: 4 }}>
+        <ContactBar />
+      </Grid>
+    </Grid>
+  );
+}
+
+export default Home;
