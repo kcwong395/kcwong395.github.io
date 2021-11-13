02b1d70566ddcc87b58fe504ba934f59bd84a1b2
Martin Wong <kcwong395@gmail.com>
2021-11-13 09:41:40 -0500

    add custom style button

---
+++ src/components/CustomButton.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import {Button} from "@mui/material";
+import {makeStyles} from "@mui/styles";
+
+const useStyles = makeStyles(({
+  btn: {
+    transition: "0.4s",
+    '&:hover': {
+      background: "#514644", //theme.palette.secondary.main,
+      color: "#efc8b1"
+    },
+  },
+}));
+
+export default function CustomButton(props: {
+  text: string;
+}) {
+
+  const classes = useStyles();
+
+  return (
+    <Button variant="contained" size="large" className={classes.btn}>{props.text}</Button>
+  )
+}
