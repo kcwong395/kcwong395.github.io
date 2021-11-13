ca0571350dcfe33543ac207d604509fbd439dc64
Martin Wong <kcwong395@gmail.com>
2021-11-13 09:39:30 -0500

    add bg banner as brief intro

---
+++ src/components/Banner.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import {Stack, Typography} from "@mui/material";
+
+import CustomButton from "./CustomButton";
+
+export default function Banner() {
+  return (
+    <Stack direction="column" spacing={{ xs: 2 }}>
+      <Typography variant="h2" component="div">
+        Martin WONG
+      </Typography>
+      <Typography variant="h4" component="div" sx={{ color: (theme) => `${theme.palette.primary.main}` }}>
+        Software Engineer
+        <br />
+      </Typography>
+      <Typography variant="body1" component="div">
+        Aspiring recent graduate seeking for challenges in <i>large-scale system design</i>
+      </Typography>
+      <CustomButton text="Read More" />
+    </Stack>
+  );
+}
