33b68d074031f9e1bcf90bb1802cb2978336cb58
Martin Wong <kcwong395@gmail.com>
2021-11-15 07:30:40 -0500

    add animation to banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,21 +1,24 @@
 import * as React from 'react';
-import {Stack, Typography} from "@mui/material";
+import {Grow, Stack, Typography} from "@mui/material";
 
 import CustomButton from "./CustomButton";
 
 export default function Banner() {
   return (
-    <Stack direction="column" spacing={{ xs: 2 }}>
-      <Typography variant="h2" component="div">
-        Martin WONG
-      </Typography>
-      <Typography variant="h4" component="div" sx={{ color: (theme) => `${theme.palette.primary.main}` }}>
-        Software Engineer
-      </Typography>
-      <Typography variant="body1" component="div">
-        Aspiring recent graduate seeking for challenges in <i>large-scale system design</i>
-      </Typography>
-      <CustomButton text="Read More" />
-    </Stack>
+    <Grow in={true} timeout={2000} style={{ transformOrigin: '0 0 0' }}>
+      <Stack direction="column" spacing={{ xs: 2 }}>
+        <Typography variant="h2" component="div">
+          Martin WONG
+        </Typography>
+        <Typography variant="h4" component="div" sx={{ color: (theme) => `${theme.palette.primary.main}` }}>
+          Software Engineer
+        </Typography>
+        <Typography variant="body1" component="div">
+          Aspiring recent graduate seeking for challenges in <i>large-scale system design</i>
+        </Typography>
+        <CustomButton text="Read More" />
+      </Stack>
+    </Grow>
+
   );
 }
