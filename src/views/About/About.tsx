9b0e729c196d74cafdeb5b55cdaff956db9c2735
Martin Wong <kcwong395@gmail.com>
2021-12-25 07:50:30 -0500

    about component init

--- src/views/About/About.tsx
+++ src/views/About/About.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import './About.css';
+import {Button, Grid} from "@mui/material";
+import CustomButton from "../../components/CustomButton";
+
 export default function About() {
   return (
-    <div>
+    <Grid container direction="column" justifyContent="space-between" id={'Section-About'} sx={{minHeight: "100vh"}}>
 
-    </div>
+    </Grid>
   );
 }
