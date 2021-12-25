b96e4aae581f265b4c22212a3ee70668ff7863a7
Martin Wong <kcwong395@gmail.com>
2021-12-25 07:53:19 -0500

    add typing effect to text

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,8 +2,41 @@ import * as React from 'react';
 import {Grow, Stack, Typography} from "@mui/material";
 
 import CustomButton from "./CustomButton";
+import Typed from 'typed.js';
 
 export default function Banner() {
+
+  // Create reference to store the DOM element containing the animation
+  const el = React.useRef(null);
+  // Create reference to store the Typed instance itself
+  const typed = React.useRef(null);
+
+  React.useEffect(() => {
+    const options = {
+      strings: [
+        'Web Technology',
+        'Cloud Transformation',
+        'Backend Development',
+        'Large-scale Distributed System'
+      ],
+      typeSpeed: 50,
+      backSpeed: 50,
+      loop: true
+    };
+
+    // elRef refers to the <span> rendered below
+    // @ts-ignore
+    typed.current = new Typed(el.current, options);
+
+    return () => {
+      // Make sure to destroy Typed instance during cleanup
+      // to prevent memory leaks
+      // @ts-ignore
+      typed.current.destroy();
+    }
+  }, [])
+
+
   return (
     <Grow in={true} timeout={2000} style={{ transformOrigin: '0 0 0' }}>
       <Stack direction="column" spacing={{ xs: 2 }}>
@@ -14,7 +47,7 @@ export default function Banner() {
           Software Engineer
         </Typography>
         <Typography variant="body1" component="div">
-          Aspiring recent graduate seeking for challenges in <i>large-scale system design</i>
+          Aspiring recent graduate seeking for challenges in <strong><i><span ref={el} /></i></strong>
         </Typography>
         <CustomButton text="Read More" />
       </Stack>
