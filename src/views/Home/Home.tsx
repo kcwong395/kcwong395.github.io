32a16678f4c297ec023d2eb5ca205c4d11b46f67
Martin Wong <kcwong395@gmail.com>
2021-12-26 01:12:04 -0500

    rename the pic and fix display position

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -8,12 +8,15 @@ import Banner from '../../components/Banner'
 
 export default function Home() {
   return (
-    <Grid container direction="column" justifyContent="space-between" id={'Section-Home'} sx={{minHeight: "100vh"}}>
-      <Grid item></Grid>
+    <Grid className={"Section-Home"} container
+          direction="column"
+          justifyContent="space-between"
+          sx={{ minHeight: "100vh" }}>
+      <Grid item />
       <Grid item>
         <Grid container>
           <Grid item xs={6} />
-          <Grid item xs={6} sx={{ pl: 10 }}>
+          <Grid item xs={6} sx={{ pl: 5, pb: 15 }}>
             <Banner />
           </Grid>
         </Grid>
