6f6544431fc9c325a3a7ea9ccfcc8c16484621da
Martin Wong <kcwong395@gmail.com>
2021-12-27 08:52:54 -0500

    navbar change background color in other pages

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,10 +3,12 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import {Divider, Grid, Link, Stack} from "@mui/material";
+import './Navbar.css';
 
 export default function Navbar() {
   return (
     <AppBar
+      className="navbar"
       position="fixed"
       color="default"
       elevation={0}
@@ -21,17 +23,17 @@ export default function Navbar() {
           <Grid item xs={5}>
             <Grid container justifyContent="space-evenly">
               <Typography variant="h5" component="div">
-                <Link href="#" color="white" underline="none" sx={{ my: 3 }}>Martin Wong</Link>
+                <Link href="#" underline="none" sx={{ my: 3 }}>Martin Wong</Link>
               </Typography>
             </Grid>
           </Grid>
           <Grid item xs={2} />
           <Grid item xs={5}>
             <Stack direction="row" divider={<Divider orientation="vertical" flexItem />} spacing={{ xs: 4 }}>
-              <Link href="#" color="white" underline="none">HOME</Link>
-              <Link href="#about" color="white" underline="none">ABOUT ME</Link>
-              <Link href="#profile" color="white" underline="none">PROFILE</Link>
-              <Link href="#project" color="white" underline="none">PROJECTS</Link>
+              <Link href="#" underline="none">HOME</Link>
+              <Link href="#about" underline="none">ABOUT ME</Link>
+              <Link href="#profile" underline="none">PROFILE</Link>
+              <Link href="#project" underline="none">PROJECTS</Link>
             </Stack>
           </Grid>
         </Grid>
